0b2dc8bcdb56f06512c1d1d6a888aa9f4c3d64ed
Luc Baudinaud <luc.baudinaud@cyclair.fr>
2025-03-18 14:32:58 +0100

    feat(tmux): reset nvim config

---
+++ install_in_container.sh
@@ -0,0 +1,9 @@
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >>~/.bashrc
+
+git clone -b pro https://github.com/Wargast/dotfiles.git ~/
+cd ~/.config/
+ln -s ~/dotfiles/nvim .
--- nvim/lua/plugins/neocolumn.lua
+++
@@ -1,6 +0,0 @@
-return {
-  {
-    "ecthelionvi/NeoColumn.nvim",
-    opts = {},
-  },
-}
