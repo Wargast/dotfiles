256d7bb97eb5d462248cbb9793a96cdf51fb1189
luc.baudinaud <luc.baudinaud@cyclair.fr>
2025-08-27 10:56:18 +0200

    feat(tmux): update tmux interface and wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,23 +1,22 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(window)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	local gui_window = window:gui_window()
+	gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
+end)
 return {
-	-- color_scheme = 'termnial.sexy',
 	color_scheme = "Catppuccin Mocha",
 	enable_tab_bar = false,
 	font_size = 11.0,
 	font = wezterm.font("JetBrains Mono"),
-	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
+	native_macos_fullscreen_mode = true,
+	audible_bell = "Disabled",
 
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
+	window_background_opacity = 0.98,
+	-- window_background_opacity = 1.0,
 	window_decorations = "RESIZE",
 	-- keys = {
 	-- 	{
