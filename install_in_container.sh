cebcf680a1fbd5b11fa77dd752ab64b9d041dd0c
luc.baudinaud <luc.baudinaud@cyclair.fr>
2025-11-04 11:12:50 +0100

    feat(tmux): update tmux interface and nvim keymaps

--- install_in_container.sh
+++ install_in_container.sh
@@ -1,11 +1,21 @@
+cd /tmp
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm nvim-linux-x86_64.tar.gz
 
-echo "export PATH=\"$PATH:/opt/nvim-linux-x86_64/bin\"" >>~/.bashrc
+echo "export PATH=\$PATH:/opt/nvim-linux-x86_64/bin" >>~/.bashrc
+
+sudo apt update
+sudo apt install -y fzf ripgrep fd-find python3.10-venv
+
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 rm -rf ~/dotfiles
-git clone -b pro https://github.com/Wargast/dotfiles.git ~/dotfiles
+git clone -b pro --depth 1 https://github.com/Wargast/dotfiles.git ~/dotfiles
 cd ~/.config/
 rm -rf nvim
 ln -sf ~/dotfiles/nvim nvim
